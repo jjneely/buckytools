32319e5c506edf579d54b55f3f3ff75463d26e97
Jack Neely <jjneely@42lines.net>
2015-06-11 15:37:51 -0400

    whisper: Fix UpdateMany with retentions count >=3
    See upstream commit:
        f62efc8a677d67b3a0e886e7278823b674a2dfeb

--- whisper/whisper.go
+++ whisper/whisper.go
@@ -400,7 +400,7 @@ func (whisper *Whisper) archiveUpdateMany(archive *archiveInfo, points []*TimeSe
 		}
 	}
 
-	higher := archive
+	higher := *archive
 	lowerArchives := whisper.lowerArchives(archive)
 
 	for _, lower := range lowerArchives {
@@ -409,7 +409,7 @@ func (whisper *Whisper) archiveUpdateMany(archive *archiveInfo, points []*TimeSe
 		for _, point := range alignedPoints {
 			interval := point.interval - mod(point.interval, lower.secondsPerPoint)
 			if !seen[interval] {
-				if propagated, err := whisper.propagate(interval, higher, &lower); err != nil {
+				if propagated, err := whisper.propagate(interval, &higher, &lower); err != nil {
 					panic("Failed to propagate")
 				} else if propagated {
 					propagateFurther = true
@@ -419,7 +419,7 @@ func (whisper *Whisper) archiveUpdateMany(archive *archiveInfo, points []*TimeSe
 		if !propagateFurther {
 			break
 		}
-		higher = &lower
+		higher = lower
 	}
 }
 
