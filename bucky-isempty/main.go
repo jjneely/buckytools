4d5a8464fe80cc7c2f349e6e247b9bfd4d8051ca
Jack Neely <jjneely@42lines.net>
2015-09-29 12:06:07 -0400

    bucky-isempty Handle WSP on CLI and report most recent DP
    If you give WSP DBs via the command line we operate on the given DB
    files.  If no files are given we do the file tree walk rooted at the
    prefix option.
    
    Added -l|-last which will report the most recent data point found.

--- bucky-isempty/main.go
+++ bucky-isempty/main.go
@@ -18,6 +18,7 @@ import (
 // Command Line Flags
 var debug bool
 var metricName bool
+var lastDP bool
 
 func usage() {
 	fmt.Printf("%s [options]\n", os.Args[0])
@@ -33,7 +34,7 @@ func usage() {
 func examine(path string, info os.FileInfo, err error) error {
 	// Did the Walk function hit an error on this file?
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s\n", err)
+		log.Printf("%s\n", err)
 		return nil
 	}
 
@@ -55,21 +56,30 @@ func examine(path string, info os.FileInfo, err error) error {
 
 	wsp, err := whisper.Open(path)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s\n", err)
+		log.Printf("%s\n", err)
 		return err
 	}
 	defer wsp.Close()
 
 	ts, count, err := buckytools.FindValidDataPoints(wsp)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s\n", err)
+		log.Printf("%s\n", err)
 		return err
 	}
 
+	if lastDP {
+		if len(ts) > 0 {
+			fmt.Printf("%s: Most recent DP: %s\t%.2f\n",
+				path, ts[0].Time, ts[0].Value)
+		} else {
+			fmt.Printf("%s: No valid data points\n", path)
+		}
+	}
 	if debug {
 		fmt.Printf("%s: %d data points used out of %d in %s\n",
 			path, len(ts), count, flag.Arg(0))
-	} else if len(ts) == 0 {
+	}
+	if !debug && !lastDP && len(ts) == 0 {
 		if metricName {
 			fmt.Println(metrics.PathToMetric(path))
 		} else {
@@ -88,6 +98,12 @@ func main() {
 	flag.BoolVar(&debug, "d", false, "Verbose output.")
 	flag.BoolVar(&metricName, "m", false,
 		"Output metric names rather than paths.")
+	flag.BoolVar(&metricName, "metricname", false,
+		"Output metric names rather than paths.")
+	flag.BoolVar(&lastDP, "l", false,
+		"Show the most recent valid data point.")
+	flag.BoolVar(&lastDP, "last", false,
+		"Show the most recent valid data point.")
 	flag.Parse()
 
 	if version {
@@ -95,9 +111,23 @@ func main() {
 		os.Exit(0)
 	}
 
-	// Start our walk
-	err := filepath.Walk(metrics.Prefix, examine)
-	if err != nil {
-		log.Fatalf("%s\n", err)
+	if flag.NArg() > 0 {
+		// Handle command line given WSP files
+		for _, p := range flag.Args() {
+			if !strings.HasSuffix(p, ".wsp") {
+				log.Fatalf("%s: Not a .wsp file", p)
+			}
+			stat, err := os.Stat(p)
+			if err != nil {
+				log.Fatalf("%s\n", err)
+			}
+			examine(p, stat, nil)
+		}
+	} else {
+		// Start our walk
+		err := filepath.Walk(metrics.Prefix, examine)
+		if err != nil {
+			log.Fatalf("%s\n", err)
+		}
 	}
 }
