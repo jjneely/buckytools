bdd99e3a0f996d81842612cb8abb7d05aa66f24e
Jack Neely <jjneely@42lines.net>
2016-01-20 11:47:23 -0500

    WIP: carbon-cache implementation
    Non-functional WIP

---
+++ buckyd/carbon.go
@@ -0,0 +1,131 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"log"
+	"net"
+	"strconv"
+)
+
+type TimeSeriesPoint struct {
+	Metric    string
+	Timestamp int64
+	Value     float64
+}
+
+func runCarbonServer(bind string) {
+	cache := runCache()
+	carbon := carbonServer(bind, stop)
+
+	for m := range carbon {
+		cache <- m
+	}
+}
+
+// carbonServer starts a TCP listener and handles incoming Graphite line
+// protocol data.
+func carbonServer(bind string) chan *TimeSeriesPoint {
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, os.Kill)
+	ln, err := net.Listen("tcp", bind)
+	if err != nil {
+		log.Fatalf("Error listening for TCP carbon connections: %s", err)
+	}
+
+	c := make(chan *TimeSeriesPoint)
+	go func() {
+		defer ln.Close()
+		defer close(c)
+		for {
+			select {
+			case <-sig:
+				log.Printf("Received signal, shutting down carbon server")
+				return
+			default:
+			}
+			ln.SetDeadline(time.Now().Add(time.Second))
+			conn, err := ln.Accept()
+			if err, ok := err.(net.Error); ok && err.Timeout() {
+				// Deadline timeout, continue loop
+				continue
+			}
+			if err != nil {
+				log.Printf("Accepting TCP connection failed: %s", err)
+				continue
+			}
+
+			go handleCarbon(conn, c)
+		}
+	}()
+
+	return c
+}
+
+func handleCarbon(conn net.Conn, c chan *TimeSeriesPoint) {
+	// Each connection has a 1KiB buffer for reading / parsing incoming data
+	buf := make([]byte, 1024, 1024)
+	offset := 0
+
+	defer conn.Close()
+	for {
+		n, err := conn.Read(buf[offset:])
+		if n > 0 {
+			lines := bytes.Split(buf[:n], []byte{'\n'})
+			last := len(lines) - 1
+			for i, line := range lines {
+				if i == last && err != io.EOF {
+					copy(buf, line)
+					offset = len(line)
+				} else {
+					dp := parseCarbonLine(line)
+					if dp != nil {
+						c <- dp
+					}
+				}
+			}
+		}
+		if err == io.EOF {
+			return
+		}
+		if err != nil {
+			log.Printf("Error reading TCP connection: %s", err)
+			return
+		}
+	}
+}
+
+func parseCarbonLine(buf []byte) *TimeSeriesPoint {
+	// XXX: Sanity check the metric name, or do we let that happen at
+	// the relay level?
+	var i int64
+	var f float64
+	var err error
+
+	fields := bytes.Split(buf, []byte{' '})
+	if len(fields) != 3 {
+		log.Printf("Illegal metric: %s", string(buf))
+		return nil
+	}
+
+	dp := new(TimeSeriesPoint)
+	dp.Metric = string(fields[0])
+	i, err = strconv.ParseInt(string(fields[1]), 10, 64)
+	if err != nil {
+		f, err = strconv.ParseFloat(string(fields[1]), 64)
+		i = int64(f)
+	}
+	if err != nil {
+		log.Printf("Illegal metric: %s", string(buf))
+		return nil
+	}
+	dp.Timestamp = i
+	f, err = strconv.ParseFloat(string(fields[2]), 64)
+	if err != nil {
+		log.Printf("Illegal metric: %s", string(buf))
+		return nil
+	}
+	dp.Value = f
+
+	return dp
+}
---
+++ buckyd/heap.go
@@ -0,0 +1,201 @@
+package main
+
+import (
+	"container/heap"
+	//"log"
+	"math"
+	"os"
+	"sort"
+)
+
+import "github.com/jjneely/buckytools/metrics"
+import "github.com/jjneely/journal"
+import "github.com/jjneely/journal/timeseries"
+
+const (
+	// MAX_CACHE is the maximum number of data points (which take 8 bytes)
+	// that we will store in the internal cache.  This should create a 1G
+	// cache.
+	MAX_CACHE = 1024 * 1024 * 1024 / 8
+)
+
+// CacheHeap is a Heap object that forms a priority queue to manage
+// writing data points to disk.
+type CacheHeap []*CacheItem
+
+// CacheStore is an ordered slice of timeseries in memory.  Sorted by
+// metric name.
+type CacheStore []*CacheItem
+
+type CacheItem struct {
+	metric string
+	index  int
+	ts     *metrics.TimeSeries // Epoch, Interval, Values
+}
+
+func (c CacheStore) Len() int {
+	return len(c)
+}
+
+func (c CacheStore) Swap(i, j int) {
+	c[i], c[j] = c[j], c[i]
+}
+
+func (c CacheStore) Less(i, j int) bool {
+	return c[i].metric < c[j].metric
+}
+
+// Search performs a binary search over the CacheStore to locate a metric.
+func (c CacheStore) Search(metric string) int {
+	cmp := func(i int) bool { return c[i].metric < metric }
+	return sort.Search(c.Len(), cmp)
+}
+
+func (c CacheHeap) Len() int {
+	return len(c)
+}
+
+func (c CacheHeap) Swap(i, j int) {
+	c[i], c[j] = c[j], c[i]
+}
+
+func (c CacheHeap) Less(i, j int) bool {
+	// This is backwards to create a MaxHeap.  We write to disk timeseries
+	// cache with the most points first.
+	return len(c[j].ts.Values) < len(c[i].ts.Values)
+}
+
+func (c *CacheHeap) Push(x interface{}) {
+	n := len(*c)
+	item := x.(*CacheItem)
+	item.index = n
+	*c = append(*c, item)
+}
+
+func (c *CacheHeap) Pop() interface{} {
+	old := *c
+	n := len(old)
+	item := old[n-1]
+	item.index = -1 // for safety
+	*c = old[0 : n-1]
+	return item
+}
+
+func (c *CacheHeap) update(item *CacheItem, dp *TimeSeriesPoint) {
+	// Adjust timestamp for regular intervals
+	timestamp := dp.Timestamp - (dp.Timestamp % item.ts.Interval)
+	i := (item.ts.Epoch - timestamp) % item.ts.Interval
+	length := int64(len(item.ts.Values)) // do casting only once
+
+	switch {
+	case i < 0:
+		// Handle change of epoch back in time
+		values := make([]float64, -i)
+		values[0] = dp.Value
+		for j := 1; j < len(values); j++ {
+			values[j] = math.NaN()
+		}
+		item.ts.Values = append(values, item.ts.Values...)
+		item.ts.Epoch = timestamp
+	case i < length:
+		item.ts.Values[i] = dp.Value
+	case i == length:
+		item.ts.Values = append(item.ts.Values, dp.Value)
+	default: // i > len(items.ts.Values)
+		values := make([]float64, i-length)
+		values[len(values)-1] = dp.Value
+		for j := 0; j < len(values)-1; j++ {
+			values[j] = math.NaN()
+		}
+		item.ts.Values = append(item.ts.Values, values...)
+	}
+
+	heap.Fix(c, item.index)
+}
+
+func newCacheItem(metric *TimeSeriesPoint) *CacheItem {
+	c := new(CacheItem)
+	ts := new(metric.TimeSeries)
+
+	ts.Epoch = metric.Timestamp
+	ts.Interval = 60 // XXX: Figure out schema
+	ts.Values = make([]float64, 1)
+	ts.Values[0] = metric.Value
+
+	c.metric = metric.Metric
+	c.ts = ts
+	return c
+}
+
+func runCache() chan *TimeSeriesPoint {
+	c := make(chan *TimeSeriesPoint)
+	cache := make(CacheHeap, 0)
+	search := make(CacheStore, 0)
+	heap.Init(&cache)
+
+	go func() {
+		defer close(c)
+		for m := range c {
+			i := search.Search(m.Metric)
+			switch {
+			case i == search.Len():
+				item := newCacheItem(m)
+				search = append(search, item)
+				heap.Push(cache, item)
+			case search[i].metric == m.Metric:
+				cache.update(search[i], m)
+			case search[i] != m.Metric:
+				item := newCacheItem(m)
+				search = append(search, nil)
+				copy(search[i+1:], search[i:])
+				search[i] = item
+				heap.Push(cache, item)
+			}
+			if len(cache[len(cache)-1].ts.Values) > int(math.Sqrt(MAX_CACHE)) {
+				item = heap.Pop(cache)
+				i = search.Search(item.metric)
+				copy(search[i:], search[i+1:])
+				evictItem(item)
+			}
+			if cache.Len() > int(math.Sqrt(MAX_CACHE)) {
+				item = heap.Pop(cache)
+				i = search.Search(item.metric)
+				copy(search[i:], search[i+1:])
+				evictItem(item)
+			}
+		}
+
+		evictAll(cache)
+	}()
+
+	return c
+}
+
+func evictItem(item *CacheItem) {
+	// XXX: If this fails it tosses metrics on the floor
+	path := metrics.MetricToPath(item.metric, ".tsj")
+	j, err := timeseries.Open(path)
+	if os.IsNotExist(err) {
+		j, err = timeseries.Create(path, ts.Interval,
+			journal.NewFloat64ValueType(), make([]int64, 0))
+	}
+	if err != nil {
+		log.Printf("Error opening/creating timeseries journal: %s", err)
+		return
+	}
+	defer j.Close()
+
+	err = metrics.JournalUpdate(j, item.ts)
+	if err != nil {
+		log.Printf("Error updating journal: %s", err)
+		return
+	}
+}
+
+func evictAll(cache CacheHeap) {
+	// XXX: If this fails it tosses metrics on the floor
+	for cache.Len() > 0 {
+		item = heap.Pop(cache).(*CacheItem)
+		evictItem(item)
+	}
+}
